407e9ef0ceb1e11555815c85199920cef8141b7f
Diego Armando Velez Peralta <davelez@pichincha.com>
2024-12-17 12:07:35 -0500

    Uso de Outputs en Angular

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output,EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
@@ -13,4 +13,11 @@ export class ProductComponent {
   @Input({required:true}) img: string = '';
   @Input() price: number = 0;
   @Input() title: string = '';
+
+  @Output() addToCar = new EventEmitter();
+
+  addToCarHandler() {
+    console.log('click desde el hijo : ' + this.title);
+    this.addToCar.emit('hola soy el padre y este es un mensaje lo acabo de recibir desde el hijo : ' + this.title);
+  }
 }
