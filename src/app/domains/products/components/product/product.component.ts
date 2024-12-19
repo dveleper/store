cb9d121cfb6aa035c603a6648d22b8ca57ef9d19
Diego Armando Velez Peralta <davelez@pichincha.com>
2024-12-19 11:46:19 -0500

    Creando componente de productos parte 2

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output,EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Product } from '../../../shared/models/product.model';
 
 
 @Component({
@@ -10,14 +11,12 @@ import { CommonModule } from '@angular/common';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  @Input({required:true}) img: string = '';
-  @Input() price: number = 0;
-  @Input() title: string = '';
+  @Input({required:true}) product!: Product;
 
   @Output() addToCar = new EventEmitter();
 
   addToCarHandler() {
-    console.log('click desde el hijo : ' + this.title);
-    this.addToCar.emit('hola soy el padre y este es un mensaje lo acabo de recibir desde el hijo : ' + this.title);
+    console.log('click desde el hijo : ' + this.product.title);
+    this.addToCar.emit('hola soy el padre y este es un mensaje lo acabo de recibir desde el hijo : ' + this.product.title);
   }
 }
