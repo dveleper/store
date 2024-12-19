9300a3654b9bf2a83ad54e4951dc215fa2f889a1
Diego Armando Velez Peralta <davelez@pichincha.com>
2024-12-19 12:12:32 -0500

    Creando el Header

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './../../components/product/product.component'
 import { Product } from './../../../shared/models/product.model'
+import { HeaderComponent } from './../../../shared/components/header/header.component'
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -35,6 +36,27 @@ export class ListComponent {
         title: 'Pro 3',
         price: 1000,
         img: 'https://picsum.photos/640/640?r=15',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 4',
+        price: 1000,
+        img: 'https://picsum.photos/640/640?r=21',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 5',
+        price: 1000,
+        img: 'https://picsum.photos/640/640?r=18',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 6',
+        price: 1000,
+        img: 'https://picsum.photos/640/640?r=35',
         creationAt: new Date().toISOString()
       }
     ];
