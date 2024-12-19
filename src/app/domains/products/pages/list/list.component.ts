5867ac50b8b61187744aa92259cbccc3c3b05057
Diego Armando Velez Peralta <davelez@pichincha.com>
2024-12-19 11:37:53 -0500

    Creando componente de productos parte 1

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './../../components/product/product.component'
+import { Product } from './../../../shared/models/product.model'
 
 @Component({
   selector: 'app-list',
@@ -10,6 +11,36 @@ import { ProductComponent } from './../../components/product/product.component'
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 1000,
+        img: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 2',
+        price: 1000,
+        img: 'https://picsum.photos/640/640?r=12',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 3',
+        price: 1000,
+        img: 'https://picsum.photos/640/640?r=15',
+        creationAt: new Date().toISOString()
+      }
+    ];
+    this.products.set(initProducts);
+  }
+
  fromChild(event: string) {
   console.log('estamos en el padre');
   console.log('evento capturado: ' + event);
